e4509dc6a6d14941c5130a5b34f6fb8f216b3e77
Misky <mial2403@student.miun.se>
2026-01-30 16:42:54 +0100

    Laboration 1_grupp50

---
+++ src/test/java/Saab95Test.java
@@ -0,0 +1,104 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Saab95Test {
+    @Test
+    void saab_startEngineSpeedToPointOne(){
+        Saab95 saab95 = new Saab95();
+        saab95.startEngine();
+        saab95.setTurboOn();
+
+        saab95.gas(1.0);
+        assertEquals(1.725, saab95.getCurrentSpeed(), 1e-9);
+    }
+
+
+      @Test
+      void saab_stopEngineSpeedToPointZero(){
+        Saab95 saab95 = new Saab95();
+        saab95.stopEngine();
+        saab95.setTurboOff();
+        assertEquals(0.0, saab95.getCurrentSpeed(), 1e-9);
+    }
+
+    @Test
+    void saab_moveDirectionEastAndSouth(){
+        Saab95 saab = new Saab95();
+        saab.startEngine();
+        saab.gas(1.0);
+        double x0 = saab.getX();
+        double y0 = saab.getY();
+        double s = saab.getCurrentSpeed();
+
+        // East
+        saab.turnRight();
+        double x1 = saab.getX();
+        double y1 = saab.getY();
+        saab.move();
+        assertEquals(x1 + s, saab.getCurrentSpeed(), 1e-9);
+        assertEquals(y1, saab.getCurrentSpeed(), 1e-9);
+
+        // South
+        saab.turnRight();
+        double x2 = saab.getX();
+        double y2 = saab.getY();
+        saab.move();
+        assertEquals(x2, saab.getCurrentSpeed(), 1e-9);
+        assertEquals(y2-s, saab.getCurrentSpeed(), 1e-9);
+
+    }
+    @Test
+    void saab_brakeTest(){
+        Saab95 saab = new Saab95();
+        saab.setCurrentSpeed(10);
+        saab.setTurboOff();
+        saab.brake(0.5);
+        assertEquals(9.375, saab.getCurrentSpeed(), 1e-9);
+
+    }
+
+    @Test
+    void saab_turnRightTest(){
+        //From NORTH to EAST
+        Saab95 saab = new Saab95();
+        saab.startEngine();
+        double xBefore = saab.getX();
+        double yBefore = saab.getY();
+        saab.turnRight();
+        assertEquals(xBefore + saab.getCurrentSpeed(), saab.getX(), 1e-9);
+        assertEquals(yBefore, saab.getY(), 1e-9);
+
+        // From EAST to SOUTH
+
+        saab.turnRight();
+        double xBefore1 = saab.getX();
+        double yBefore1 = saab.getY();
+        saab.move();
+        assertEquals(xBefore, saab.getX(), 1e-9);
+        assertEquals(yBefore1 - saab.getCurrentSpeed(), saab.getY(), 1e-9);
+    }
+
+    @Test
+    void saab_turnLeftTest(){
+
+        // From NORTH to WEST
+        Saab95 saab = new Saab95();
+        saab.startEngine();
+        double xBefore = saab.getX();
+        double yBefore = saab.getY();
+        saab.move();
+        assertEquals(xBefore - saab.getCurrentSpeed(), saab.getX(), 1e-9);
+        assertEquals(yBefore, saab.getY(), 1e-9);
+
+        
+        // From WEST to SOUTH
+        saab.turnLeft();
+        double xBefore1 = saab.getX();
+        double yBefore1 = saab.getY();
+        saab.move();
+        assertEquals(xBefore, saab.getX(), 1e-9);
+        assertEquals(yBefore1 - saab.getCurrentSpeed(), saab.getY(), 1e-9);
+    }
+
+}
---
+++ src/test/java/Volvo240Test.java
@@ -0,0 +1,101 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Volvo240Test {
+    @Test
+
+    void volvo_startEngineSpeedToPointOne(){
+        Volvo240 car = new Volvo240();
+        car.startEngine();
+        car.gas(1.0);
+        assertEquals(1.35, car.getCurrentSpeed(), 1e-9);
+    }
+    @Test
+    void volvo_stopEngineSpeedToPointZero(){
+        Volvo240 car = new Volvo240();
+        car.startEngine();
+        car.stopEngine();
+        assertEquals(0.0, car.getCurrentSpeed(), 1e-9);
+    }
+
+    @Test
+    void volvo_move_coversEastAndSouth(){
+        Volvo240 car = new Volvo240();
+        car.startEngine();
+        car.gas(1.0);
+        double x0 = car.getX();
+        double y0 = car.getY();
+        double s = car.getCurrentSpeed();
+
+        //East
+        car.turnRight();
+        double x1 = car.getX();
+        double y1 = car.getY();
+        car.move();
+        assertEquals(x1 + s, car.getX(), 1e-9);
+        assertEquals(y1, car.getY(), 1e-9);
+
+        //South
+        car.turnRight();
+        double x2 = car.getX();
+        double y2 = car.getY();
+        car.move();
+        assertEquals(x2, car.getX(), 1e-9);
+        assertEquals(y2-s, car.getY(), 1e-9);
+    }
+
+    @Test
+    void volvo_brakeTest(){
+        Volvo240 car = new Volvo240();
+        car.setCurrentSpeed(15);
+        car.brake(0.8);
+        assertEquals(14, car.getCurrentSpeed(), 1e-9);
+    }
+
+    @Test
+    void volvo_turnRightTest(){
+        // From NORTH to EAST
+        Volvo240 car = new Volvo240();
+        car.startEngine();
+        double xBefore = car.getX();
+        double yBefore = car.getY();
+        car.turnRight();
+        car.move();
+        assertEquals(xBefore + car.getCurrentSpeed(), car.getX(), 1e-9);
+        assertEquals(yBefore, car.getY(), 1e-9);
+
+        //From EAST to SOUTH
+        car.turnRight();
+        double xBefore2 = car.getX();
+        double yBefore2 = car.getY();
+        car.move();
+        assertEquals(xBefore2, car.getX(), 1e-9);
+        assertEquals(yBefore2 - car.getCurrentSpeed(), car.getY(), 1e-9);
+
+
+    }
+
+    @Test
+    void volvo_turnLeftTest(){
+        // From NORTH to WEST
+
+        Volvo240 car = new Volvo240();
+        car.startEngine();
+        double xBefore = car.getX();
+        double yBefore = car.getY();
+        car.turnLeft();
+        car.move();
+        assertEquals(xBefore - car.getCurrentSpeed(), car.getX(), 1e-9);
+        assertEquals(yBefore, car.getY(), 1e-9);
+
+        //From WEST to SOUTH
+        car.turnLeft();
+        double xBefore1 = car.getX();
+        double yBefore1 = car.getY();
+        car.move();
+        assertEquals(xBefore1, car.getX(), 1e-9);
+        assertEquals(yBefore1 - car.getCurrentSpeed(), car.getY(), 1e-9);
+    }
+
+}
